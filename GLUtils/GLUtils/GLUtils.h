41679a7fe3c2d345f0899740898ed6a6d478f0ca
Gyeolee <gyeoleeeeeee@gmail.com>
2022-07-25 16:50:43 +0900

    GLUtils, iOS-Example project 및 Log struct 추가

---
+++ GLUtils/GLUtils/GLUtils.h
@@ -0,0 +1,18 @@
+//
+//  GLUtils.h
+//  GLUtils
+//
+//  Created by Hangyeol on 2022/07/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GLUtils.
+FOUNDATION_EXPORT double GLUtilsVersionNumber;
+
+//! Project version string for GLUtils.
+FOUNDATION_EXPORT const unsigned char GLUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GLUtils/PublicHeader.h>
+
+
